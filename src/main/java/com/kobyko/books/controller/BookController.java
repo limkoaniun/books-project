12010e4718ccca94faac657056771d9cf450dedd
Guanyu Lin <z5472236@ad.unsw.edu.au>
2025-07-12 18:51:45 +1000

    Add GET /api/v2/books endpoint with optional category filter

--- src/main/java/com/kobyko/books/controller/BookController.java
+++ src/main/java/com/kobyko/books/controller/BookController.java
@@ -3,6 +3,7 @@ package com.kobyko.books.controller;
 import com.kobyko.books.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -18,12 +19,35 @@ public class BookController {
     }
 
     private void initialiseBooks() {
-        books.addAll(List.of(new Book("The Hobbit", "J.R.R. Tolkien", "Fantasy"), new Book("To Kill a Mockingbird", "Harper Lee", "Fiction"), new Book("1984", "George Orwell", "Dystopian"), new Book("The Great Gatsby", "F. Scott Fitzgerald", "Classic"), new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "Fantasy")));
+        books.addAll(
+                List.of(
+                        new Book("The Hobbit", "J.R.R. Tolkien", "Fantasy"),
+                        new Book("To Kill a Mockingbird", "Harper Lee", "Fiction"),
+                        new Book("1984", "George Orwell", "Dystopian"),
+                        new Book("The Great Gatsby", "F. Scott Fitzgerald", "Classic"),
+                        new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "Fantasy")
+                )
+        );
     }
 
     @GetMapping("/api/v1/books")
     public List<Book> getAllBooks() {
         return books;
+    }
+
+    @GetMapping("/api/v2/books")
+    public List<Book> getBooksByCategory(@RequestParam(required = false) String category) {
+        if(category == null || category.isEmpty()) {
+            return books;
+        }
+
+        List<Book> filteredBooks = new ArrayList<>();
+        for (Book book : books) {
+            if(book.getCategory().equalsIgnoreCase(category)) {
+                filteredBooks.add(book);
+            }
+        }
+        return filteredBooks;
     }
 
     @GetMapping("/api/v1/book/{title}")
